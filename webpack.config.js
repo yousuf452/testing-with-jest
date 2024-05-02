35768bc7da1421154a1ec5fb73444f390b7d7a12
Yousuf Saboor <119664309+yousuf452@users.noreply.github.com>
2024-05-02 16:22:43 +0200

    lade till webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const CopyWebpackPlugin = require(copy-webpack-plugin);
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [{
+                context: __dirname + '/src/',
+                from: '*.html'
+            }]
+        })
+    ]
+};
+
