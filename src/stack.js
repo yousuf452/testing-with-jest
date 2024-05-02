9ad2a821b0d4dcac6cf12725f24b5d77145673a0
Yousuf Saboor <119664309+yousuf452@users.noreply.github.com>
2024-05-02 16:57:44 +0200

    selenium tester

---
+++ src/stack.js
@@ -0,0 +1,18 @@
+const _ = require('underscore');
+
+let stack = [];
+
+// Lägger ett element överst i stacken
+exports.push = function (x) {
+    stack.push(x);
+};
+
+// Returnerar det översta elementet i stacken och tar bort det
+exports.pop = function () {
+    return stack.pop();
+}
+
+// Returnerar det översta elementet i stacken
+exports.peek = function () {
+    return _.last(stack);
+}
